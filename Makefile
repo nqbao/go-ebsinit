e9977f17a6823de25b784668e95cfb16f1374337
Bao Nguyen <b@nqbao.com>
2019-03-28 14:23:27 -0700

    Add version flag

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
+VERSION = v0.1
+
 all: clean linux
 
 linux:
-	GOOS=linux go build -o ebsinit_linux_amd64
+	GOOS=linux go build -ldflags="-X main.version=$(VERSION) -s -w" -o ebsinit_linux_amd64
 
 clean:
 	rm -f ebsinit* go-ebsinit*
--- main.go
+++ main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"time"
 )
 
 var (
-	label   string
-	format  string
-	timeout int
-	mount   string
+	label       string
+	format      string
+	timeout     int
+	mount       string
+	showVersion bool
 )
+
+var version = "0.1"
 
 func main() {
 	flag.StringVar(&label, "label", "DATA", "Target disk label")
 	flag.StringVar(&mount, "mount", "/data", "Mount location")
 	flag.StringVar(&format, "format", "ext4", "Target disk format")
 	flag.IntVar(&timeout, "timeout", 30, "How long in seconds to wait for the EBS to come up")
+	flag.BoolVar(&showVersion, "version", false, "Show version")
 
 	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("Version: %v\n", version)
+		return
+	}
 
 	log.Printf("Look for device with label %v with timeout %d seconds", label, timeout)
 
